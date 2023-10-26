ca2a082269070dcc6eabe6ae2b02c47cf1323918
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2023-10-26 20:27:18 -0300

    Componentização

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,6 +1,7 @@
 import {Text, View} from "react-native"
 import { styles } from "./styles"
 import Header from "../../components/Header"
+import Card from "../../components/Card"
 
 
 function Home(){
@@ -9,7 +10,30 @@ function Home(){
         <View>
            <Text style= {styles.title}>JOGOS</Text>
 
-           <Header rodada="ª Rodada"/>
+           <Header rodada="29ª Rodada"/>
+           <View>
+                <Card stadium="Arena Grêmio"
+                    date="25/ 10 - ONtem - 21:30"
+                    teamHome="Grêmio"
+                    scoreHome="3"
+                    teamVisited="Flamengo"
+                    scoreVisited="2"
+                    />
+                    <Card stadium="Arena Palmeiras"
+                    date="25/ 10 - Ontem - 21:30"
+                    teamHome="Palmeiras"
+                    scoreHome="5"
+                    teamVisited="São Paulo"
+                    scoreVisited="0"
+                    />
+                    <Card stadium="Arena Grêmio"
+                    date="25/ 10 - ONtem - 21:30"
+                    teamHome="Grêmio"
+                    scoreHome="3"
+                    teamVisited="Flamengo"
+                    scoreVisited="2"
+                    />
+           </View>
         </View>
     )
 }
